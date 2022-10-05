0edccb3983d250e52cdc143919daef8f7d6d0ec2
Marcel-Programador <backing.audio@gmail.com>
2022-10-04 22:38:28 -0300

    Aplicando partials head e footer e tambem midlleware 'log'.

--- app.js
+++ app.js
@@ -3,13 +3,19 @@ const app = express();
 const homeRoute = require("./src/routes/homeRoute");
 const productsRoute = require("./src/routes/productsRoute");
 const port = 3000;
+const logMiddleware = require("./middlewares/log")
 
 app.set("view engine", "ejs");
 app.set("views", __dirname + "/src/views");
 app.use(express.static(__dirname + "/public"));
+app.use(logMiddleware)
 
 app.use(homeRoute);
 app.use(productsRoute);
+
+app.use((req, res) => {
+    return res.status(404).send("Solicitação não encontrada")
+})
 
 app.listen(port, () => {
     console.log(`Servidor rodando no endereço http://localhost:${port}`)
