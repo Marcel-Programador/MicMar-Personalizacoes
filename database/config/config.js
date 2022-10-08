9483ff5bded52d5d8181ac9fd12d3cc3bc6c51fa
Marcel-Programador <backing.audio@gmail.com>
2022-10-08 15:19:05 -0300

    Iniciano implementação 'Sequelize'.

---
+++ database/config/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  "development": {
+      "username": "root",
+      "password": "",
+      "database": "micmar",
+      "host": "127.0.0.1",
+      "dialect": "mysql",
+      "operatorsAliases": false
+  }
+};
