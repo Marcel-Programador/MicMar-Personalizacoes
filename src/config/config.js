c5cd1584612fb73b9fa4a1361652436ffefbb1a4
Marcel-Programador <backing.audio@gmail.com>
2022-10-09 20:23:49 -0300

    Implementado Sequelize inicial na homePage.

---
+++ src/config/config.js
@@ -0,0 +1,9 @@
+const config = {
+    username: "root",
+    password: "",
+    database: "micmar",
+    host: "localhost",
+    dialect: "mysql",
+};
+
+module.exports = config
--- src/controllers/HomeController.js
+++ src/controllers/HomeController.js
@@ -1,7 +1,15 @@
+const { Homehcl, Homehca, MainSection } = require("../models")
+
 const HomeController = {
-    home: ((req, res) => {
-        return res.render("homePage", {title: "MICMAR | HOME"});
+    home: async(req, res) => {
+        let hcl = await Homehcl.findOne()
+        console.log(hcl)
+        let hca = await Homehca.findOne()
+        console.log(hca)
+        let ms = await MainSection.findOne()
+        console.log(ms)
+        return res.render("homePage", {title: "MICMAR | HOME", hcl, hca, ms});
     }
-)};
+};
 
 module.exports = HomeController
