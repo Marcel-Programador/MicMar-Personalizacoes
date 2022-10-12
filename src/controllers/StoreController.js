e6592dcdf3bef68d1d9c24c4f32eb187e2cfa654
Marcel-Programador <backing.audio@gmail.com>
2022-10-12 16:55:55 -0300

    Implementando novas views, modificações no controller StoreController para nova rota(routestoreRoute).

--- src/controllers/StoreController.js
+++ src/controllers/StoreController.js
@@ -1,34 +1,45 @@
-const { ProductsModels } = require("../models");
+const { HomehclModels, HomehcaModels, ProductsModels } = require("../models");
 
 const StoreController = {
     store: async (req, res) => {
-        const { category, mark, type, color, quantity, costPrice, salePrice, specialPrice, description, img } = req.body;
+        const { category, mark, type, color, quantity, costPrice, salePrice, specialPrice, description } = req.body;
+        let filename = "shoes-defaut.png";
 
-        if ( !category || !mark || !type || !color || !quantity || !costPrice || !salePrice || !specialPrice || !description || !img ){
-            return res.send({message: "Chegou até aqui pelo menos"})
+
+        if (req.file) {
+          filename = req.file.filename
+        }
+
+        if ( !category || !mark || !type || !color || !quantity || !costPrice || !salePrice || !specialPrice || !description ){
+            return res.send({message: "Se chegou aqui é porque esqueceu de cadastrar alguma informação"})
         }
 try {
-        const product = await ProductsModels.create({
-                category: category,
-                mark: mark,
-                type: type,
-                color: color,
-                quantity: parseInt(quantity),
-                costPrice: parseFloat(costPrice),
-                salePrice: parseFloat(salePrice),
-                specialPrice: parseFloat(specialPrice),
-                description: description,
-                img: img
-              })
+    const product = await ProductsModels.create({
+            category: category,
+            mark: mark,
+            type: type,
+            color: color,
+            quantity: parseInt(quantity),
+            costPrice: parseFloat(costPrice),
+            salePrice: parseFloat(salePrice),
+            specialPrice: parseFloat(specialPrice),
+            description: description,
+            img: filename
+          })
 
-              var products = await ProductsModels.findAll()
+    return res.send({title: product}).status(200);
 
-        return res.send({title: product, products: products});
-
-            } catch (error) {
-                return res.send({message: "erro"})
-            }
+        } catch (error) {
+            return res.send({message: "erro"})
         }
+    },
+    view: async (req, res) => {
+        let hcl = await HomehclModels.findOne();
+        // console.log(hcl);
+        let hca = await HomehcaModels.findOne();
+        // console.log(hca);
+        return res.render("productRegistration", {title: "MICMAR | CADASTRO DE PRODUTOS", hcl, hca});
     }
+}
 
 module.exports = StoreController
--- src/routes/storeRoute.js
+++ src/routes/storeRoute.js
@@ -1,7 +1,22 @@
 const express = require("express");
 const router = express.Router();
 const StoreController = require("../controllers/StoreController");
+const path = require("path");
+const multer = require("multer");
 
-router.post("/store/products", StoreController.store);
+const multerDiskStorage = multer.diskStorage({
+    destination: (req, file, callback) => {
+        callback(null, "./uploads");
+    },
+    filename: (req, file, callback) => {
+        const imageName = Date.now() + path.extname(file.originalname);
+        callback(null, imageName);
+    }
+});
+
+const upload = multer({storage: multerDiskStorage});
+
+router.get("/store/registration", StoreController.view);
+router.post("/store/products", upload.single("avatar"), StoreController.store);
 
 module.exports = router;
